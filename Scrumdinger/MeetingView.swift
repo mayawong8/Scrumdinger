55403a5560d9efa44d16b347025d853216fc176d
Maya <mayajwong8@gmail.com>
2023-05-18 23:54:15 -0400

    added accessibility function

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -22,7 +22,10 @@ struct MeetingView: View {
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
-        
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityLabel("10 minutes")
+            
             Circle().strokeBorder(lineWidth: 24)
             HStack{
                 Text("Speaker 1 of 3")
@@ -30,6 +33,7 @@ struct MeetingView: View {
                 Button(action: {}){
                     Image(systemName: "forward.fill")
                 }
+                .accessibilityLabel("Next speaker")
             }
         }
         .padding()
